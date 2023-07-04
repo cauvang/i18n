1088e4aeda2396385992ca3b687a3d8c8c9c6611
Thao Pham <thao.pham@levo.com.au>
2023-07-05 00:27:23 +1000

    fix: return 404 main

--- src/app/[lang]/home/pageContent.tsx
+++ src/app/[lang]/home/pageContent.tsx
@@ -3,6 +3,8 @@
 import DropDownList from '@/components/dropdown'
 import DynamicContent from '@/components/dynamicContent'
 import StaticContent from '@/components/staticContent'
+import { useRootPathname } from '@/hook/usePath'
+import { notFound } from 'next/navigation'
 import { useState } from 'react'
 
 export default function HomePage() {
@@ -12,9 +14,12 @@ export default function HomePage() {
       ? 'bg-blue-500 hover:bg-blue-700 text-white border border-blue-700'
       : ' hover:bg-offWhite text-grey'
 
+  const { pathName, isHavingLocale } = useRootPathname()
+  if (!isHavingLocale && pathName != '/') return notFound()
+
   return (
     <main className='p-7'>
-      {/* <div className='flex justify-between'>
+      <div className='flex justify-between'>
         <span className='bg-white shadow-btn-white p-0.5 inline-flex cursor-pointer rounded-full'>
           <button
             className={`rounded-full py-2 px-4 gap-2.5 font-bold ${activeButton(
@@ -39,7 +44,7 @@ export default function HomePage() {
 
       {contentType === 'dynamic' && <DynamicContent />}
 
-      {contentType === 'static' && <StaticContent />} */}
+      {contentType === 'static' && <StaticContent />}
     </main>
   )
 }
